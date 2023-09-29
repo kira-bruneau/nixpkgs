58723f3c4ebc76b3008156676aa0ac2cbfd0597f
Kira Bruneau <kira.bruneau@pm.me>
2023-09-29 11:29:34 -0400

    wlprop: fetchgit -> fetchFromGitHub

--- pkgs/tools/wayland/wlprop/default.nix
+++ pkgs/tools/wayland/wlprop/default.nix
@@ -1,13 +1,14 @@
-{ fetchgit, gawk, jq, lib, makeWrapper, slurp, stdenv, sway, bash }:
+{ fetchFromGitHub, gawk, jq, lib, makeWrapper, slurp, stdenv, sway, bash }:
 
 stdenv.mkDerivation rec {
   pname = "wlprop";
   version = "unstable-2022-08-18";
 
-  src = fetchgit {
-    url = "https://gist.github.com/f313386043395ff06570e02af2d9a8e0";
+  src = fetchFromGitHub {
+    owner = "crispyricepc";
+    gist = "f313386043395ff06570e02af2d9a8e0";
     rev = "758c548bfb4be5b437c428c8062b3987f126f002";
-    sha256 = "sha256-ZJ9LYYrU2cNYikiVNTlEcI4QXcoqfl7iwk3Be+NhGG8=";
+    hash = "sha256-ZJ9LYYrU2cNYikiVNTlEcI4QXcoqfl7iwk3Be+NhGG8=";
   };
 
   strictDeps = true;
