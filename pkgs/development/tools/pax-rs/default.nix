e0117dd70b19c61fbfd043de4fb6fda50a6f2b06
Kira Bruneau <kira.bruneau@pm.me>
2023-09-29 11:34:04 -0400

    pax-rs: fetchurl -> fetchFromGitHub

--- pkgs/development/tools/pax-rs/default.nix
+++ pkgs/development/tools/pax-rs/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, fetchurl, rustPlatform, runCommand } :
+{ lib, fetchFromGitHub, rustPlatform, runCommand } :
 
 rustPlatform.buildRustPackage rec {
   pname = "pax-rs";
@@ -22,19 +22,21 @@ rustPlatform.buildRustPackage rec {
         owner = "nathan";
         repo = "pax";
         rev = "pax-v${version}";
-        sha256 = "1l2xpgsms0bfc0i3l0hyw4dbp6d4qdxa9vxyp704p27vvn4ndhv2";
+        hash = "sha256-YsNmid37iEvAub7vpHrDpJm7GuEeAjoiYG4BXfW7XdA=";
       };
 
-      cargo-lock = fetchurl {
-        url = "https://gist.github.com/klntsky/c7863424d7df0c379782015f6bb3b399/raw/1cf7481e33984fd1510dc77ed677606d08fa8eb6/Cargo.lock";
-        sha256 = "0ff1b64b99cbca1cc2ceabcd2e4f7bc3411e3a2a9fbb9db2204d9240fe38ddeb";
+      cargo-lock = fetchFromGitHub {
+        owner = "klntsky";
+        gist = "c7863424d7df0c379782015f6bb3b399";
+        rev = "1cf7481e33984fd1510dc77ed677606d08fa8eb6";
+        hash = "sha256-oSDTAstGBKfeMSqiSA0fcm07BifpQj1ugKbxK86N+uY=";
       };
     in
     runCommand "pax-rs-src" {} ''
       cp -R ${source} $out
       chmod +w $out
-      cp ${cargo-lock} $out/Cargo.lock
+      cp ${cargo-lock}/Cargo.lock $out
     '';
 
-  cargoSha256 = "0d6g52hjflnw2zvlx10pz78527vh7mw5n43yi8q6dwr3pkbds1fs";
+  cargoHash = "sha256-2gXd1rwj82Ywin4QW3g9cB9R0PkXhE73F9xSJ6EozzQ=";
 }
