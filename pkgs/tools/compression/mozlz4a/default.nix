b4abccf0b01cbec76d83e4c85da018fd1f62a14f
Kira Bruneau <kira.bruneau@pm.me>
2023-09-29 11:49:00 -0400

    mozlz4a: fetchurl -> fetchFromGitHub

--- pkgs/tools/compression/mozlz4a/default.nix
+++ pkgs/tools/compression/mozlz4a/default.nix
@@ -1,19 +1,19 @@
-{ lib, stdenv, fetchurl, python3, runtimeShell }:
+{ lib, stdenv, fetchFromGitHub, python3, runtimeShell }:
 
 stdenv.mkDerivation rec {
   pname = "mozlz4a";
   version = "2018-08-23";
-  # or fetchFromGitHub(owner,repo,rev) or fetchgit(rev)
-  src = fetchurl {
-    url = "https://gist.githubusercontent.com/kaefer3000/73febe1eec898cd50ce4de1af79a332a/raw/a266410033455d6b4af515d7a9d34f5afd35beec/mozlz4a.py";
-    sha256 = "1d1ai062kdms34bya9dlykkx011rj8d8nh5l7d76xj8k9kv4ssq6";
-  };
 
-  dontUnpack = true;
+  src = fetchFromGitHub {
+    owner = "kaefer3000";
+    gist = "73febe1eec898cd50ce4de1af79a332a";
+    rev = "a266410033455d6b4af515d7a9d34f5afd35beec";
+    hash = "sha256-fAgDDX8waDRzx/THOb+iINAwmkvAq7rfLb17rN/7+5o=";
+  };
 
   installPhase = ''
     mkdir -p "$out/bin" "$out/${python3.sitePackages}/"
-    cp "${src}" "$out/${python3.sitePackages}/mozlz4a.py"
+    cp "${src}/mozlz4a.py" "$out/${python3.sitePackages}"
 
     echo "#!${runtimeShell}" >> "$out/bin/mozlz4a"
     echo "export PYTHONPATH='$PYTHONPATH'" >> "$out/bin/mozlz4a"
@@ -28,6 +28,6 @@ stdenv.mkDerivation rec {
     license = lib.licenses.bsd2;
     maintainers = [lib.maintainers.raskin lib.maintainers.pshirshov lib.maintainers.kira-bruneau];
     platforms = lib.platforms.unix;
-    homepage = "https://gist.githubusercontent.com/Tblue/62ff47bef7f894e92ed5";
+    homepage = "https://gist.github.com/Tblue/62ff47bef7f894e92ed5";
   };
 }
