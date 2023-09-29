8bd19cdb402ad8a3609fbac425d9e1363df952df
Kira Bruneau <kira.bruneau@pm.me>
2023-09-29 11:49:17 -0400

    ecryptfs-helper: fetchurl -> fetchFromGitHub

--- pkgs/tools/security/ecryptfs/helper.nix
+++ pkgs/tools/security/ecryptfs/helper.nix
@@ -1,5 +1,5 @@
 { lib, stdenv
-, fetchurl
+, fetchFromGitHub
 , makeWrapper
 , python2
 }:
@@ -8,19 +8,19 @@ stdenv.mkDerivation {
   pname   = "ecryptfs-helper";
   version = "20160722";
 
-  src = fetchurl {
-    url    = "https://gist.githubusercontent.com/obadz/ec053fdb00dcb48441d8313169874e30/raw/4b657a4b7c3dc684e4d5e3ffaf46ced1b7675163/ecryptfs-helper.py";
-    sha256 = "0gp4m22zc80814ng80s38hp930aa8r4zqihr7jr23m0m2iq4pdpg";
+  src = fetchFromGitHub {
+    owner = "obadz";
+    gist = "ec053fdb00dcb48441d8313169874e30";
+    rev = "4b657a4b7c3dc684e4d5e3ffaf46ced1b7675163";
+    hash = "sha256-VIpUYs2ZZLn7IRzQSwHlM1hiJXjwGVyrrJZt7CSdn58=";
   };
-
-  dontUnpack = true;
 
   nativeBuildInputs = [ makeWrapper ];
 
   # Do not hardcode PATH to ${ecryptfs} as we need the script to invoke executables from /run/wrappers/bin
   installPhase = ''
     mkdir -p $out/bin $out/libexec
-    cp $src $out/libexec/ecryptfs-helper.py
+    cp $src/ecryptfs-helper.py $out/libexec
     makeWrapper "${python2.interpreter}" "$out/bin/ecryptfs-helper" --add-flags "$out/libexec/ecryptfs-helper.py"
   '';
 
